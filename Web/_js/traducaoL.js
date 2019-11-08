58cc7f358e677bd5bd748a0930242832ccb28445
leosaloa <43081870+leosaloa@users.noreply.github.com>
2019-11-08 03:03:48 -0300

    Adicionado a tradução por letras e barra de espaço

---
+++ Web/_js/traducaoL.js
@@ -0,0 +1,162 @@
+function espaco(){
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/espaco.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+}
+function letraAL() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_p/a-p.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+}
+  function letraBL() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_p/b-p.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+}
+function letraCL() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_p/c-p.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+}
+function letraDL() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_p/d-p.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+}
+function letraEL() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_p/e-p.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+}
+function letraFL() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_p/f-p.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+}
+function letraGL() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_p/g-p.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+}
+function letraHL() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_p/h-p.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+}
+function letraIL() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_p/i-p.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+}
+function letraJL() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_p/j-p.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+}
+function letraKL() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_p/k-p.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+}
+function letraLL() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_p/l-p.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+}
+function letraML() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_p/m-p.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+}
+function letraNL() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_p/n-p.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+}
+function letraOL() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_p/o-p.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+}
+function letraPL() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_p/p-p.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+}
+function letraQL() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_p/q-p.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+}
+function letraRL() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_p/r-p.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+}
+function letraSL() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_p/s-p.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+}
+function letraTL() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_p/t-p.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+}
+function letraUL() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_p/u-p.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+}
+function letraVL() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_p/v-p.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+}
+function letraWL() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_p/w-p.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+}
+function letraXL() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_p/x-p.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+}
+function letraYL() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_p/y-p.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+}
+function letraZL() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_p/z-p.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+}
---
+++ Web/_js/traducaoP.js
@@ -0,0 +1,164 @@
+function espaco(){
+  var x = document.createElement("IMG");
+  x.setAttribute("src", "../_img/espaco.png");
+  document.body.appendChild(x);
+  document.getElementById('area').appendChild(x);  
+}
+function letraAP() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_l/a-l.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+  }
+  function letraBP() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_l/b-l.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+  }
+  function letraCP() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_l/c-l.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+  }
+  function letraDP() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_l/d-l.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+  }
+  function letraEP() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_l/e-l.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+  }
+  function letraFP() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_l/f-l.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+  }
+  function letraGP() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_l/g-l.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+  }
+  function letraHP() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_l/h-l.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+  }
+  function letraIP() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_l/i-l.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+  }
+  function letraJP() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_l/j-l.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+  }
+  function letraKP() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_l/k-l.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+  }
+  function letraLP() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_l/l-l.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+  }
+  function letraMP() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_l/m-l.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+  }
+
+  function letraNP() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_l/n-l.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+  }
+  function letraOP() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_l/o-l.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+  }
+  function letraPP() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_l/p-l.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+  }
+  function letraQP() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_l/q-l.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+  }
+  function letraRP() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_l/r-l.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+  }
+  function letraSP() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_l/s-l.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+  }
+  function letraTP() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_l/q-l.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+  }
+  function letraUP() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_l/u-l.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+  }
+  function letraVP() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_l/v-l.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+  }
+  function letraWP() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_l/w-l.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+  }
+  function letraXP() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_l/x-l.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+  }
+  function letraYP() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_l/y-l.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+  }
+  function letraZP() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "../_img/alfabeto_l/z-l.png");
+    document.body.appendChild(x);
+    document.getElementById('area').appendChild(x);
+  }
+  
